e9e04d05145f502fbf782df5cef1d86c2ec62245
M Haidar Hanif <git@mhaidarhanif.com>
2024-05-15 21:11:11 +0700

    feat: display all initial habits

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
+import { dataHabits } from "./data/habits";
+
 export function App() {
+  const name = "M Haidar Hanif";
+
   return (
-    <div>
-      <h1>Habitat 2</h1>
+    <div className="m-10 flex justify-center">
+      <main className="w-full max-w-3xl space-y-4">
+        <h1 className="text-5xl">Habitat for {name}</h1>
+
+        <hr />
+
+        <section className="space-y-2">
+          <h2 className="text-2xl">My Habits</h2>
+
+          <div>
+            <ul className="space-y-1">
+              {dataHabits.map((habit) => (
+                <li>
+                  <p>{habit.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/data/habits.ts
@@ -0,0 +1,17 @@
+export const dataHabits = [
+  {
+    id: 1,
+    text: "Breakfast",
+    category: "Food",
+  },
+  {
+    id: 2,
+    text: "Jogging",
+    category: "Sport",
+  },
+  {
+    id: 3,
+    text: "Lunch",
+    category: "Food",
+  },
+];
